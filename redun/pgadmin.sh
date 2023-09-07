e66d5450bac77de0a661d7131033d884bbcf648d
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2023-09-07 19:57:26 +0300

    persist pgadmin data

--- redun/pgadmin.sh
+++ redun/pgadmin.sh
@@ -1,7 +1,9 @@
 docker rm -f phppgadmin
+mkdir -p pg_admin_data
 docker run --name='phppgadmin' -d \
   --net host \
     -e 'PGADMIN_DEFAULT_EMAIL=root@root.com' \
     -e 'PGADMIN_DEFAULT_PASSWORD=root' \
     -e 'PGADMIN_LISTEN_PORT=9999' \
+    -v "./pg_admin_data:/var/lib/pgadmin" \
 dpage/pgadmin4
